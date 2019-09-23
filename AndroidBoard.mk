331cf73e4d912c2ccdeb8aa4502cd690756e4d49
Dennis Murimi Wachira <d.murimi14@gmail.com>
2019-09-23 20:16:40 +0300

    Initial TECNO Spark 3 Pro TWRP Tree Upload

---
+++ AndroidBoard.mk
@@ -0,0 +1,5 @@
+LOCAL_PATH := $(call my-dir)
+
+ifneq ($(filter KB8,$(TARGET_DEVICE)),)
+include $(call all-makefiles-under,$(LOCAL_PATH))
+endif
---
+++ omni_KB8.mk
@@ -0,0 +1,25 @@
+# Release name
+PRODUCT_RELEASE_NAME := KB8
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_BRAND := TECNO
+PRODUCT_DEVICE := KB8
+PRODUCT_MANUFACTURER := TECNO MOBILE LIMITED
+PRODUCT_MODEL := TECNO KB8
+PRODUCT_NAME := omni_KB8
+
+# enable stock zip packages flash
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.treble.enabled=true \
+    persist.sys.usb.config=mtp \
+    persist.service.adb.enable=1 \
+    persist.service.debuggable=1 \
+    ro.secure=1 \
+    ro.adb.secure=0 \
+    ro.allow.mock.location=0
